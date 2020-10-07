5fee360c17ad7a4c0eddce7410c8d9ad012f7b6f
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-06 22:45:13 -0500

    Add routes got Shelter creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+
+  post 'shelters', to: 'shelters#create'
 end
