9ba890fe905586dcfea2d50b04fa3dd9685c44c4
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-07 09:33:29 -0500

    Update shelter controller to show list of shelters

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,6 +1,6 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['shelter 1', 'shelter 2']
+    @shelters = Shelter.all
   end
 
   def new
@@ -8,10 +8,10 @@ class SheltersController < ApplicationController
 
   def create
     shelter = Shelter.new({
-      name:    params[:shelter][:name]
-      address: params[:shelter][:address]
-      city:    params[:shelter][:city]
-      state:   params[:shelter][:state]
+      name:    params[:shelter][:name],
+      address: params[:shelter][:address],
+      city:    params[:shelter][:city],
+      state:   params[:shelter][:state],
       zip:     params[:shelter][:zip]
       })
 
