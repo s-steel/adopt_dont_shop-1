5f67e7b0aca056a3d7709e10d2092fcd4e4921db
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-07 16:45:31 -0500

    Add delete functionality for shelters

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
-
+  get '/shelters/:id/delete', to: 'shelters#destroy'
   post 'shelters', to: 'shelters#create'
-
   patch 'shelters/:id', to: 'shelters#update'
+  # delete 'shelters/:id', to: 'shelters#destroy'
 end
