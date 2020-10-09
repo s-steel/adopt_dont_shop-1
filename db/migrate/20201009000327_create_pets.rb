385b37d792925edb38a23a98e451342ba285ccab
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-08 19:06:50 -0500

    Create Pets table

---
+++ db/migrate/20201009000327_create_pets.rb
@@ -0,0 +1,6 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_141720) do
+ActiveRecord::Schema.define(version: 2020_10_09_000327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
