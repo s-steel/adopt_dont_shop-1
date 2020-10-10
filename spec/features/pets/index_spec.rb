324d855be65dab33ec35d66ea72adb6e73495100
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-10 13:46:21 -0500

    Add feature test for shelter pet creation

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 describe 'pet index page' do
+  let!(:shelter1) { create(:shelter, name: 'Great Test Shelter') }
+  let!(:shelter2) { create(:shelter, name: 'Cool Test Shelter') }
+  let!(:pet1) { create(:pet, name: 'Taylor', approximate_age: 6, sex: 'Male', shelter_id: shelter1.id) }
+  let!(:pet2) { create(:pet, name: 'Edna', approximate_age: 8, sex: 'Female', shelter_id: shelter2.id) }
+  
+  before do
+    visit'/pets'
+  end
+
   it 'can see title' do
-    visit '/pets'
     expect(page).to have_content('All Pets') 
   end
 
   it 'can see pets' do
-    shelter1 = create(:shelter, name: 'Great Test Shelter')
-    shelter2 = create(:shelter, name: 'Cool Test Shelter')
-    pet1 = create(:pet, name: 'Taylor', approximate_age: 6, sex: 'Male', shelter_id: shelter1.id)
-    pet2 = create(:pet, name: 'Edna', approximate_age: 8, sex: 'Female', shelter_id: shelter2.id)
-
-    visit '/pets'
     expect(page).to have_link(pet1.name)
     expect(page).to have_link(pet2.name)
 
