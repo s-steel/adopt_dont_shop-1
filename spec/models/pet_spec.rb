c46c9a93abccf3d521c12342bfc4edf7d4b4bd78
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-11 17:48:58 -0500

    Add test for validating the presence of a name for shelters and pets

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Pet, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+  end
+
   describe 'relationships' do
     it { should belong_to :shelter }
   end
