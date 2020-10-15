e02236e448f3a1095cd89ac14ca9e2b7c31051a5
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-15 16:43:41 -0500

    Create UserApplications and ApplicationPets tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_134417) do
+ActiveRecord::Schema.define(version: 2020_10_15_193758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "application_pets", force: :cascade do |t|
+    t.bigint "user_application_id"
+    t.bigint "pet_id"
+    t.index ["pet_id"], name: "index_application_pets_on_pet_id"
+    t.index ["user_application_id"], name: "index_application_pets_on_user_application_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "image"
@@ -45,6 +52,13 @@ ActiveRecord::Schema.define(version: 2020_10_13_134417) do
     t.integer "zip"
   end
 
+  create_table "user_applications", force: :cascade do |t|
+    t.string "description"
+    t.string "status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_user_applications_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -53,7 +67,10 @@ ActiveRecord::Schema.define(version: 2020_10_13_134417) do
     t.integer "zip"
   end
 
+  add_foreign_key "application_pets", "pets"
+  add_foreign_key "application_pets", "user_applications"
   add_foreign_key "pets", "shelters"
   add_foreign_key "reviews", "shelters"
   add_foreign_key "reviews", "users"
+  add_foreign_key "user_applications", "users"
 end
