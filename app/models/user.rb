30c41804502b4d48a66b16b1995f3ecad0a0702f
Sean Steel <seanrsteel@gmail.com>
2020-10-15 07:59:30 -0600

    Create tests for user show feature with highlighted reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,14 @@ class User < ApplicationRecord
   end
 
   def review_best_rating
+    return nil if reviews.empty?
+
     reviews.order(rating: :desc).limit(1).first
   end
 
   def review_worst_rating
+    return nil if reviews.empty?
+
     reviews.order(:rating).limit(1).first
   end
 end
