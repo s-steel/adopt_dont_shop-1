d26beeec87e2b91fb11b48f7a658a36902a05223
Sean Steel <seanrsteel@gmail.com>
2020-10-20 15:04:17 -0600

    Create methods for the shelter statistics, user story 32

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,28 @@
 class Shelter < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  has_many :pets, dependent: :restrict_with_exception
+  has_many :pets, dependent: :destroy
 
   validates_presence_of :name
+
+  def pet_count
+    pets.count
+  end
+
+  def average_review_rating
+    return 0 if reviews.empty?
+
+    reviews.average(:rating)
+  end
+
+  def number_applications
+    pets.sum do |pet|
+      pet.application_count
+    end
+  end
+
+  def pending_pets?
+    pets.find_all do |pet|
+      pet.approved_application?
+    end.empty?
+  end
 end
