736a260bd917e0a657d7edd7a659a46327de355e
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-20 17:51:08 -0500

    Refactor shelters/show_spec.rb

--- spec/factories/shelters.rb
+++ spec/factories/shelters.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :shelter do
     name { Faker::Company.name + " Shelter" }
+
+    trait :with_reviews do
+      transient do
+        review_count { 3 }
+      end
+
+      after(:create) do |shelter, evaluator|
+        shelter.reviews << create_list(:review, evaluator.review_count)
+      end
+    end
+
+    trait :with_pets do
+      transient do
+        pet_count { 3 }
+      end
+
+      after(:create) do |shelter, evaluator|
+        shelter.pets << create_list(:pet, evaluator.pet_count)
+      end
+    end
   end
 end
